e04ea8d0572d3d5984a2fa5a7457a7c5f52ad2f1
SwalveSam <samantha.swalve@gmail.com>
2021-07-19 13:08:47 -0500

    updated app.js and pet.js with .jsx

--- src/app.js
+++ src/app.js
@@ -1,26 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import Pet from "./pet";
 
+
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!!"),
-    React.createElement(Pet, {
-      name: "Birchy",
-      animal: "Cat",
-      breed: "Orange and white Stripes",
-    }),
-    React.createElement(Pet, {
-      name: "Koda",
-      anmial: "Dog",
-      breed: "american eskimo",
-    }),
-    React.createElement(Pet, {
-      name: "Phish",
-      animal: "Bunny",
-      breed: "white rabbit",
-    }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+      <Pet name="Birch" animal="Cat" breed="Orange and while stripes"></Pet>
+      <Pet name="Koda" animal="Dog" breed="American Eskimo"></Pet>
+      <Pet name="Phish" animal="Bunny" breed="cow like"></Pet>
+    </div>
+  )
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));

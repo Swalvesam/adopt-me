32734ad5d6bcde6f4613f5b51749f111dd40dbd0
SwalveSam <samantha.swalve@gmail.com>
2021-07-20 10:38:45 -0500

    added custom hook

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import ReactDOM from 'react-dom';
-import Pet from "./pet";
+// import Pet from "./pet";
+import SearchParams from './SearchParams';
 
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <Pet name="Birch" animal="Cat" breed="Orange and while stripes"></Pet>
+      <SearchParams />
+      {/* <Pet name="Birch" animal="Cat" breed="Orange and while stripes"></Pet>
       <Pet name="Koda" animal="Dog" breed="American Eskimo"></Pet>
-      <Pet name="Phish" animal="Bunny" breed="cow like"></Pet>
+      <Pet name="Phish" animal="Bunny" breed="cow like"></Pet> */}
     </div>
   )
 }
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
